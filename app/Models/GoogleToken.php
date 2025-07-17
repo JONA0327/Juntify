2b9642fcfbd2a08a151e33f9da619ce82208d07d
JONA0327 <106278316+JONA0327@users.noreply.github.com>
2025-07-17 14:38:39 -0600

    Add Google Drive integration

---
+++ app/Models/GoogleToken.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GoogleToken extends Model
+{
+    public $timestamps = true;
+    protected $fillable = [
+        'username',
+        'access_token',
+        'refresh_token',
+        'expiry_date',
+        'recordings_folder_id',
+    ];
+}
