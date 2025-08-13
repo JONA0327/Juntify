6258861feed2e873fd78ed028a63a6b9f14a5245
JONA0327 <106278316+JONA0327@users.noreply.github.com>
2025-08-13 14:37:30 -0600

    feat: add modal button class

--- resources/js/reuniones_v2.js
+++ resources/js/reuniones_v2.js
@@ -1107,13 +1107,13 @@ function showEditNameModal(meetingId, currentName) {
                     </div>
 
                     <div class="modal-actions">
-                        <button class="action-btn secondary" onclick="closeEditNameModal()">
+                        <button class="modal-btn secondary" onclick="closeEditNameModal()">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                             Cancelar
                         </button>
-                        <button class="action-btn primary" onclick="confirmEditMeetingName(${meetingId})">
+                        <button class="modal-btn primary" onclick="confirmEditMeetingName(${meetingId})">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                             </svg>
@@ -1273,13 +1273,13 @@ function showDeleteConfirmationModal(meetingId) {
                     </div>
 
                     <div class="modal-actions">
-                        <button class="action-btn secondary" onclick="closeDeleteConfirmationModal()">
+                        <button class="modal-btn secondary" onclick="closeDeleteConfirmationModal()">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                             Cancelar
                         </button>
-                        <button class="action-btn danger" onclick="confirmDeleteMeeting(${meetingId})">
+                        <button class="modal-btn danger" onclick="confirmDeleteMeeting(${meetingId})">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                             </svg>
