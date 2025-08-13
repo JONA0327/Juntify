d935b07e7f77921d79590ecd6e8229ab12237372
JONA0327 <106278316+JONA0327@users.noreply.github.com>
2025-08-13 14:23:23 -0600

    refactor: usar icon-btn en acciones de reuniones

--- resources/js/reuniones_v2.js
+++ resources/js/reuniones_v2.js
@@ -517,12 +517,12 @@ function createMeetingCard(meeting) {
                 </div>
 
                 <div class="meeting-actions">
-                    <button class="action-btn edit-btn" onclick="editMeetingName(${meeting.id})" title="Editar nombre de reunión">
+                    <button class="icon-btn edit-btn" onclick="editMeetingName(${meeting.id})" title="Editar nombre de reunión">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                         </svg>
                     </button>
-                    <button class="action-btn delete-btn" onclick="deleteMeeting(${meeting.id})" title="Eliminar reunión">
+                    <button class="icon-btn delete-btn" onclick="deleteMeeting(${meeting.id})" title="Eliminar reunión">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
