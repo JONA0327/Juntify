1106a333286175547238a196f223357fdcc6ac3d
JONA0327 <106278316+JONA0327@users.noreply.github.com>
2025-08-18 13:21:55 -0600

    Add shared meetings and containers endpoints with tests

---
+++ app/Models/MeetingContainer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class MeetingContainer extends Model
+{
+    protected $fillable = [
+        'username',
+        'name',
+    ];
+
+    public function meetings(): BelongsToMany
+    {
+        return $this->belongsToMany(TranscriptionLaravel::class, 'container_meetings', 'container_id', 'meeting_id');
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,8 @@ Route::middleware('auth')->group(function () {
 
     // Rutas API para reuniones
     Route::get('/api/meetings', [MeetingController::class, 'getMeetings'])->name('api.meetings');
+    Route::get('/api/shared-meetings', [MeetingController::class, 'getSharedMeetings'])->name('api.shared-meetings');
+    Route::get('/api/containers', [MeetingController::class, 'getContainers'])->name('api.containers');
     Route::get('/api/meetings/{id}', [MeetingController::class, 'show'])->name('api.meetings.show');
     Route::put('/api/meetings/{id}/name', [MeetingController::class, 'updateName'])->name('api.meetings.updateName');
     Route::put('/api/meetings/{id}/segments', [MeetingController::class, 'updateSegments'])->name('api.meetings.updateSegments');
