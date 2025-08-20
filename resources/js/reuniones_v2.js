fef864bcf2ef66ea2963af69726e9339601e1187
JONA0327 <106278316+JONA0327@users.noreply.github.com>
2025-08-20 12:49:34 -0600

    Expose container select modal functions

--- resources/js/reuniones_v2.js
+++ resources/js/reuniones_v2.js
@@ -2609,6 +2609,8 @@ window.openEditContainerModal = openEditContainerModal;
 window.closeContainerModal = closeContainerModal;
 window.saveContainer = saveContainer;
 window.deleteContainer = deleteContainer;
+window.openContainerSelectModal = openContainerSelectModal;
+window.closeContainerSelectModal = closeContainerSelectModal;
 
 // Funciones del modal de reuniones del contenedor
 window.openContainerMeetingsModal = openContainerMeetingsModal;
