2afe26be5b05e89e3aa74dcbbcaa7280ede98dd1
JONA0327 <106278316+JONA0327@users.noreply.github.com>
2025-09-05 11:50:25 -0600

    feat: support webm audio uploads

--- resources/views/partials/new-meeting/_audio-uploader.blade.php
+++ resources/views/partials/new-meeting/_audio-uploader.blade.php
@@ -4,8 +4,8 @@
         <x-icon name="folder" class="upload-icon" />
         <h3 class="upload-title">Arrastra y suelta tu archivo de audio aquí</h3>
         <p class="upload-subtitle">O haz clic para seleccionar un archivo de audio</p>
-        <p class="upload-formats">Formatos soportados: MP3, WAV, M4A, FLAC, OGG, AAC</p>
-        <input type="file" id="audio-file-input" accept=".mp3,.wav,.m4a,.flac,.ogg,.aac,audio/*" style="display: none;">
+        <p class="upload-formats">Formatos soportados: MP3, WAV, M4A, FLAC, OGG, AAC, WEBM</p>
+        <input type="file" id="audio-file-input" accept=".mp3,.wav,.m4a,.flac,.ogg,.aac,.webm,audio/*" style="display: none;">
         <button class="btn btn-primary upload-btn">
             <x-icon name="folder" class="btn-icon" />
             Seleccionar archivo
