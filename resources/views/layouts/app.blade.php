819055ed7bf8c07fb2b87670d65bc4d20d24fbf7
Jonathan Israel Loredo Palacios <Jona03278@gmail.com>
2025-10-15 14:52:33 -0600

    Replace favicon asset with inline data URI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,11 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    @php
+        $inlineFavicon = 'data:image/svg+xml;base64,' . base64_encode('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><rect width="64" height="64" rx="12" fill="#0f172a"/><path fill="#38bdf8" d="M20 12h8v24a8 8 0 0 0 16 0h8c0 13.255-10.745 24-24 24S4 49.255 4 36V12h8v24c0 8.837 7.163 16 16 16s16-7.163 16-16V12h8v24a24 24 0 0 1-48 0z"/></svg>');
+    @endphp
+    <link rel="icon" type="image/svg+xml" href="{{ $inlineFavicon }}">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:300,400,500,600,700&display=swap" rel="stylesheet" />
