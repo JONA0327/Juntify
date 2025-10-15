3c52e538ed09a12eaee0aad45ae1c8d1e1c6f9da
Jonathan Israel Loredo Palacios <Jona03278@gmail.com>
2025-10-15 14:16:10 -0600

    Display seeded plan data in profile plans

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            PlansSeeder::class,
+        ]);
+
         TranscriptionLaravel::factory(10)->create();
     }
 }
