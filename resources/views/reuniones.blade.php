bcc9d8476086502bd0e727dc296f0f7a960e6402
Jonathan Israel Loredo Palacios <Jona03278@gmail.com>
2025-10-23 15:32:36 -0600

    Move contacts management into organization page

--- resources/views/reuniones.blade.php
+++ resources/views/reuniones.blade.php
@@ -96,9 +96,6 @@
                             <button class="tab-transition px-4 py-2 rounded-lg bg-slate-800/50 border border-slate-700/50 text-slate-200 hover:bg-slate-700/50" data-target="shared-meetings">Reuniones compartidas</button>
                         </li>
                         <li>
-                            <button class="tab-transition px-4 py-2 rounded-lg bg-slate-800/50 border border-slate-700/50 text-slate-200 hover:bg-slate-700/50" data-target="contacts">Contactos</button>
-                        </li>
-                        <li>
                             <button class="tab-transition px-4 py-2 rounded-lg bg-slate-800/50 border border-slate-700/50 text-slate-200 hover:bg-slate-700/50" data-target="containers">Contenedores</button>
                         </li>
                     </ul>
@@ -142,10 +139,6 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div id="contacts" class="hidden">
-                        @include('contacts.index', ['hideChat' => true])
                     </div>
 
                     <div id="containers" class="hidden">
