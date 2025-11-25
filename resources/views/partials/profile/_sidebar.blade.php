673cb5951000c3b3e0b0a651f13ff746628bb22b
JONA0327 <jona03278@gmail.com>
2025-11-25 16:17:46 -0600

    Corregir Juntify

--- resources/views/partials/profile/_sidebar.blade.php
+++ resources/views/partials/profile/_sidebar.blade.php
@@ -45,14 +45,6 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="{{ route('tutorial.settings') }}" class="nav-link">
-                    <svg class="nav-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    Tutorial
-                </a>
-            </li>
-            <li class="nav-item">
                 <a href="#" class="nav-link" data-section="about">
                     <svg class="nav-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25h1.5m-1.5 0v5.25m.75-9.75h.008v.008H12" />
